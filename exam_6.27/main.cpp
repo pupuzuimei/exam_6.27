a7cfb7d1799b4cb2b71f9d8a8bd5158e97b82a5a
pupuzuimei <2977759369@qq.com>
2019-06-27 12:05:29 +0800

    tr
    yt

--- exam_6.27/main.cpp
+++ exam_6.27/main.cpp
@@ -1,15 +1,71 @@
-//
-//  main.cpp
-//  exam_6.27
-//
-//  Created by s20181106278 on 2019/6/27.
-//  Copyright © 2019 s20181106278. All rights reserved.
-//
-
 #include <iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+struct student{
+    int number;
+    string name;
+    string sex;
+    int age;
+};
+struct people{
+    string referee;
+    string people;
+    int score[5];
+};
+int main() {
+    student t[10];
+    people k[10];
+    int i,j;
+    ofstream inf("/Users/s20181106278/Desktop/myinf.txt");
+    ifstream onf("/Users/s20181106278/Desktop/onf.txt");
+    ifstream enf("/Users/s20181106278/Desktop/enf.txt");
+    if(onf.is_open())
+    {
+        for(i=0;i<5;++i)
+        {
+            onf>>t[i].number>>t[i].name>>t[i].sex>>t[i].age;
+        }
+        onf.close();
+    }
+    if(enf.is_open())
+    {
+        for(j=1;j<6;j++)
+        {
+            enf>>k[j].people;
+        }
+        for(j=1;j<8;++j)
+        {
+            for(i=1;i<6;++i)
+            {
+              enf>>k[j].score[i];
+            }
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+        }
+        
+        enf.close();
+    }
+    if(inf.is_open())
+    {
+        for(int i=0;i<5;i++)
+        {
+            inf<<t[i].number<<" "<<t[i].name<<" "<<t[i].sex<<" "<<t[i].age<<endl;
+            
+        }
+        for(int i=1;i<6;i++)
+        {
+            inf<<k[i].people;
+        }
+        inf<<endl;
+        for(j=1;j<8;++j)
+        {
+            for(i=1;i<6;++i)
+            {
+                inf<<k[j].score[i];
+            }
+            
+        }
+        inf.close();
+    }
     return 0;
 }
